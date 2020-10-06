5c04b5d18dd361fe0c7a167d55c05002e4ec4bea
AlexLiu <alexsote@outlook.com>
2020-10-06 11:15:22 +0800

    画了一只猫

--- src/views/sections/defs/Defs.page.js
+++ src/views/sections/defs/Defs.page.js
@@ -1,10 +1,11 @@
 import React from "react";
-import "./style.css";
+import styles from "./style.module.css";
+console.log(styles)
 function Defs() {
   const mouses = (
     <svg xmlns="https://www.w3.org/2000/svg"
       viewBox="0 0 390 330"
-      className="svg">
+      className={styles.svg}>
       <defs>
         <g id="mouse">
           {/* 脸 */}
@@ -13,14 +14,14 @@ function Defs() {
           <circle cx={50} cy={50} r={45} />
           {/* 眼睛 */}
           <circle cx={30} cy={35} r={10} fill="white" />
-          <circle cx={30} cy={37} r={3} fill="black" />
+          <circle cx={30} cy={40} r={3} fill="black" />
           <circle cx={70} cy={35} r={10} fill="white" />
-          <circle cx={70} cy={37} r={3} fill="black" />
+          <circle cx={70} cy={40} r={3} fill="black" />
           {/* 鼻子 */}
-          <line x1={50.5} y1={50} x2={50.5} y2={70} stroke="blue"/>
+          <line x1={50.5} y1={50} x2={50.5} y2={70} stroke="blue" />
           <circle cx={50} cy={50} r={3} fill="blue" />
           {/* 嘴巴 */}
-          <ellipse cx={50} cy={73} rx={15} ry={3} fill="red"/>
+          <ellipse cx={50} cy={73} rx={15} ry={3} fill="red" />
         </g>
       </defs>
       <use href="#mouse" className="mouse" x={10} y={-20} fill="black" />
@@ -34,10 +35,38 @@ function Defs() {
       <use href="#mouse" className="mouse" x={270} y={250} fill="black" />
     </svg>
   );
-  return <div className="svgs">
+  const tomAndJerry = (
+    <svg xmlns="https://www.w3.org/2000/svg"
+      viewBox="0 0 390 330"
+      className={styles.svg}>
+      <defs>
+        <g id="tom">
+          <polyline points="0,80 -40,150 -50,150" stroke-width="3" fill="transparent" />
+          <polyline points="0,80 10,150 0,150" stroke-width="3" fill="transparent" />
+          <polyline points="110,80 80,150 70,150" stroke-width="3" fill="transparent" />
+          <polyline points="110,80 140,150 130,150" stroke-width="3" fill="transparent" />
+          <polyline points="110,80 230,20" stroke-width="3" />
+          <ellipse cx={50} cy={73} rx={100} ry={40} />
+          {/* 头 */}
+          <circle cx={-50} cy={27} r={50} />
+          <circle cx={-70} cy={20} r={10} fill="white" />
+          <circle cx={-68} cy={15} r={4} fill="black" />
+          <circle cx={-30} cy={20} r={10} fill="white" />
+          <circle cx={-28} cy={15} r={4} fill="black" />
+          <ellipse cx={-50} cy={50} rx={15} ry={3} fill="red" />
+          <polyline points="-100,20 -120,-30 -70,-10" stroke-width="3" />
+          <polyline points="0,20 20,-30 -30,-10" stroke-width="3" />
+        </g>
+      </defs>
+      <use href="#tom" x={150} y={120} fill="blue" stroke="blue" />
+    </svg>
+  );
+  return <div className={styles.svgs}>
     <div>{mouses}{mouses}{mouses}</div>
     <div>{mouses}{mouses}{mouses}</div>
     <div>{mouses}{mouses}{mouses}</div>
+
+    <div>{tomAndJerry}</div>
   </div>;
 }
 export default Defs;
